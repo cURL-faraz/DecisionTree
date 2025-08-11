e2d8399c7b96dd104b0d2dae24dbe1cc51588d1a
Faraz Ameri <amerifaraz84@gmail.com>
2025-08-11 16:52:04 +0330

    feat: Complete data preprocessing pipeline with feature engineering

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,10 +6,23 @@ import numpy as np
 
 from sklearn.decomposition import PCA
 
+def detect_categories(dataset):
+    
+    column_categories = {}
+    
+    for column in dataset.columns :
+        if dataset[column].nunique() <= 10 :
+            column_categories[column] = list(dataset[column].unique())
+        else :
+            column_categories[column] = None 
+
+    return column_categories
+
 def load_dataset():
     
     dataset = pd.read_csv("../data/Airplane.csv" , index_col = 0)
-    return dataset 
+    column_categories = detect_categories(dataset)
+    return dataset , column_categories
 
 def encode_feats(dataset):
 
@@ -21,10 +34,11 @@ def encode_feats(dataset):
             categories = dict([(categories[i] , i) for i in range(len(categories))])
             dataset[feat] = dataset[feat].apply(lambda x : categories[x])
 
-def drop_feats(dataset , feats_list):
+def drop_feats(dataset , feats_list , column_categories):
     
     for feat in feats_list:
         dataset = dataset.drop(feat , axis = 1)
+        del column_categories[feat]
 
 def numerical_fillna(dataset , feats_list):
     
@@ -36,20 +50,22 @@ def skew_reduction(dataset , feats_list):
     for feat in feats_list :
         dataset[feat] = boxcox(dataset[feat]+1)[0]
 
-def quantile_binning(dataset , feats_dict):
+def quantile_binning(dataset , feats_dict , column_categories):
     
     for feat in feats_dict.keys() :
         k = feats_dict[feat]
         quantiles = dataset[feat].quantile(np.linspace(0,1,k+1)).values 
         quantiles = np.unique(quantiles)
+        column_categories[feat] = quantiles.tolist()
         dataset[feat] = (np.searchsorted(quantiles , dataset[feat] , side = "right") - 1 ).astype(int)
         dataset[feat] = np.clip(dataset[feat] , 0 , k-1)
 
-def equal_width_binning(dataset , feats_dict):
+def equal_width_binning(dataset , feats_dict , column_categories):
     
     for feat in feats_dict.keys():
         k = feats_dict[feat]
         bins = np.linspace(dataset[feat].min() , dataset[feat].max() , k+1)
+        column_categories[feat] = bins.tolist()
         dataset[feat] = (np.searchsorted(bins , dataset[feat] , side = "right") - 1).astype(int)
 
 def merge_by_mean(dataset , feats_list):
@@ -74,8 +90,7 @@ def merge_by_pca(dataset , feats_list):
     super_feat = pd.Series((pca.fit_transform(sub_df)).flatten())
     return super_feat
 
-
-def merge_into_super_feat(dataset , feats_dict):
+def merge_into_super_feat(dataset , feats_dict , column_categories):
     
     for feats_tuple in feats_dict.keys():
         method = feats_dict[feats_tuple][0]
@@ -84,31 +99,18 @@ def merge_into_super_feat(dataset , feats_dict):
             dataset[super_feat_name] = merge_by_mean(feats_tuple)
         else :
             dataset[super_feat_name] = merge_by_pca(feats_tuple)
-        
+        quantile_binning(dataset , {super_feat_name : 4} , column_categories)
         drop_feats(dataset , feats_tuple)
 
 
 def dataset_preprocessing(drop_feats_list,numerical_fillna_feats_list,skewed_feats_list,quantile_feats_dict,equal_width_feats_dict,merging_feats_dict):
     
-    dataset = load_dataset()
-    drop_feats(dataset , drop_feats_list)
+    dataset , column_categories = load_dataset()
+    drop_feats(dataset , drop_feats_list , column_categories)
     encode_feats(dataset)
     numerical_fillna(dataset , numerical_fillna_feats_list)
     skew_reduction(dataset , skewed_feats_list)
-    quantile_binning(dataset , quantile_feats_dict)
-    equal_width_binning(dataset , equal_width_feats_dict)
-    merge_into_super_feat(dataset , merging_feats_dict)
-    return dataset
-
-
-
-
-
-
-
-
-
-
-
-
-
+    quantile_binning(dataset , quantile_feats_dict , column_categories)
+    equal_width_binning(dataset , equal_width_feats_dict , column_categories)
+    merge_into_super_feat(dataset , merging_feats_dict , column_categories)
+    return dataset , column_categories
